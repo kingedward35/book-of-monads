b5c84f4e6b5237b66df9665b00ce16589eeb23d7
Verlon Smith <verlonsmith@Verlons-MacBook-Pro.local>
2019-09-16 20:26:15 -0600

    Finished with Chapter 0

--- bookOfMonads/src/Chapter0/Chapter0.hs
+++ bookOfMonads/src/Chapter0/Chapter0.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 instance (Eq a, Eq b) => Eq (a, b) where
   a == b = True
   _ == _ = False
@@ -9,3 +11,17 @@ class Container c where
 instance Container [] where
   empty = []
   insert x xs = x : xs
+
+-- data Queue a =
+--   Queue
+--     { unQueue :: [a]
+--     }
+newtype Queue a =
+  Queue
+    { unQueue :: [a]
+    }
+
+instance Container Queue where
+  empty = Queue []
+  insert x (Queue xs) = Queue (xs <> [x])
+  -- insert x xs = Queue (unQueue xs <> [x])
