dfed806dc9ed0802198c376062b8cea5f2cfba92
Verlon Smith <verlonsmith@Verlons-MacBook-Pro.local>
2019-09-16 20:14:30 -0600

    Creating a Container instance for List

--- bookOfMonads/src/Chapter0/Chapter0.hs
+++ bookOfMonads/src/Chapter0/Chapter0.hs
@@ -1,3 +1,11 @@
 instance (Eq a, Eq b) => Eq (a, b) where
   a == b = True
   _ == _ = False
+
+class Container c where
+  empty :: c a
+  insert :: a -> c a -> ca
+
+instance Container [] where
+  empty = []
+  insert x xs = x : xs
